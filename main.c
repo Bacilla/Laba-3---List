122abef74ab6d8dcbbb9bf51ff24066aefe16257
Никита Левашов <n.levashov@inbox.ru>
2023-09-03 03:15:34 +0300

    Added function "print_all_data". The "add_to_end" function is fully working.

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #define NAME_LENGTH 50
 
 // Структура данных о фильме
-typedef struct SFilm {
+typedef struct {
     char name[NAME_LENGTH];
     int session;
     int price;
@@ -13,44 +13,38 @@ typedef struct SFilm {
 } Film;
 
 // Структура элемента односвязного списка
-typedef struct SList {
-    struct SList *next;
+typedef struct SList List;
+struct SList {
+    List *next;
     Film data;
-} List;
+};
 
 
-
+// Функция выводит на экран информацию о конкретной структуре(Film)
 void print_film_data() {
 
 }
 
 // Прототипы функций
 int get_int();
-void add_to_end(List *start_p, List *end_p);
+void add_to_end(List **ppStart, List **ppEnd);
 Film fill_film_data();
+void print_all_data(List *start_p);
 
 
 int main() {
-    // Указатель на первый элемент списка
-    List *start_p = NULL;
-    // Указатель на последний списка
-    List *end_p = NULL;
+    // Указатель на первый элемент списка (он всегда будет указывать на начало списка)
+    List *pStart = NULL;
+    // Указатель на последний элемент списка (он всегда будет смещаться по мере добавления или удаления элементов)
+    List *pEnd = NULL;
+ 
+    add_to_end(&pStart, &pEnd);
+    add_to_end(&pStart, &pEnd);
+    add_to_end(&pStart, &pEnd);
 
-    add_to_end(start_p, end_p);
+    print_all_data(pStart);
 
 
-
-
-
-
-
-    // printf("Input your string:");
-    // char my_string[NAME_LENGTH];
-    // if(fgets(my_string, NAME_LENGTH, stdin) == NULL) {
-    //     printf("Error in fgets()\n");
-    //     return 0;
-    // }
-    // printf("%s", my_string);
 
 
     return 0;
@@ -69,19 +63,32 @@ int get_int() {
 	return value;
 }
 
-void add_to_end(List *start_p, List *end_p) {
+// Функция добавляет новый элемент в конец списка
+void add_to_end(List **ppStart, List **ppEnd) {
     // Создание новой структуры Film
     Film new_film;
     // Заполнение структуры данными
     new_film = fill_film_data();
 
-    printf("%s\n", new_film.name);
-    printf("%d\n", new_film.session);
-    printf("%d\n", new_film.price);
-    printf("%d\n", new_film.num_of_viewers);
+    // Создание нового элемента
+    List *new_element = malloc(sizeof(List));
+    // Заполнение структуры данными
+    (*new_element).next = NULL; // Т.к. элемент добавляется в конец списка
+    (*new_element).data = new_film; // Заполняем элемент списка данными о фильме
 
+   
+    // Если это не добавление первого элемента в список и указатель pEnd уже указывает на какой-то элемент,
+    // то связываем элемент, на который указывает указатель с новым последним элементом
+    if((*ppEnd) != NULL) {
+        (*ppEnd)->next = new_element; // Теперь бывший последний элемент(который хранился по адресу в переменной pEnd) больше не является последним
+    }
+    // Переприсваиваем указатель на новый последний элемент 
+    *ppEnd = new_element;
 
-
+    // Если это первый элемент в списке - указатель начала списка теперь указывает на него
+    if((*ppStart) == NULL) {
+        *ppStart = new_element;
+    }
 }
 
 // Функция создаёт новую структру Film и инициализирует её пользовательскими данными
@@ -117,4 +124,21 @@ Film fill_film_data() {
     f.num_of_viewers = get_int();
 
     return f;
+}
+
+// Функция выводит информацию о каждом фильме
+void print_all_data(List *pStart) {
+    List *temp = pStart;
+    int i = 1; // номер элемента в списке
+    printf("--START OF THE LIST--\n");
+    while(temp != NULL) {
+        printf("\tElement %d:\n", i);
+        printf("name:\t\t%s\n", (*temp).data.name);
+        printf("session:\t%d\n", (*temp).data.session);
+        printf("price:\t\t%d\n", (*temp).data.price);
+        printf("vievers:\t%d\n", (*temp).data.num_of_viewers);
+        temp = (*temp).next;
+        i++;
+    }
+    printf("---END OF THE LIST---\n");
 }
